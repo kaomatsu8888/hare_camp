7e615e4e093be912fa8ad0b1fd0310b8ce672250
kaomatsu8888 <m.kaoru0830@gmail.com>
2023-11-08 12:17:17 +0900

    ”背景変更とりあえずコミット

--- resources/views/campgrounds/index.blade.php
+++ resources/views/campgrounds/index.blade.php
@@ -1,19 +1,21 @@
 {{-- resources/views/campgrounds/index.blade.php --}}
 <x-app-layout>
-    <div class="container mx-auto mt-8">
-        <h1 class="text-xl mb-4">キャンプ場一覧</h1>
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white border-b border-gray-200">
-                <ul class="list-disc space-y-2">
-                    @foreach ($campgrounds as $campground)
-                        <li>
-                            <a href="{{ route('campgrounds.show', $campground) }}" class="text-blue-500 hover:underline">
-                                {{ $campground->name }}
-                            </a>
-                            <p>価格: ¥{{ number_format($campground->price) }}</p>
-                        </li>
-                    @endforeach
-                </ul>
+    <div class="bg-campground"> <!-- このdivに背景画像クラスを適用する -->
+        <div class="container mx-auto mt-8 opacity-75"> <!-- 背景画像にコンテンツを重ねるために透明度を下げる -->
+            <h1 class="text-xl mb-4 ">キャンプ場一覧</h1>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <ul class="list-disc space-y-2">
+                        @foreach ($campgrounds as $campground)
+                            <li>
+                                <a href="{{ route('campgrounds.show', $campground) }}" class="text-blue-500 hover:underline">
+                                    {{ $campground->name }}
+                                </a>
+                                <p>価格: ¥{{ number_format($campground->price) }}</p>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- ... 他のリンクやスタイル ... -->
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,8 +17,10 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    {{-- <body class="font-sans antialiased"> --}}
+    <body class="font-sans antialiased background-image">
+
+        {{-- <div class="min-h-screen bg-gray-100"> --}}
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -31,6 +36,6 @@
             <main>
                 {{ $slot }}
             </main>
-        </div>
+        {{-- </div> --}}
     </body>
 </html>
